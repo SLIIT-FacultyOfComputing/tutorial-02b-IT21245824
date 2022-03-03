7d35916e5c6caf675beedb8a93f60c9a88efdf7c
D.A. Hashan Madusnaka <99629335+IT21245824@users.noreply.github.com>
2022-03-03 16:45:16 +0000

    Tute 1,2,3,4 completed

--- tute03.cpp
+++ tute03.cpp
@@ -4,20 +4,24 @@ Convert the C program given below which calculates the Factorial of a number tha
 
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
-{
-    int no;
-    long fac;
+#include<iostream>  //preprocessive directive
+using namespace std;
 
-    printf("Enter a Number : ");
-    scanf("%d", &no);
+int main(){
 
-    fac = 1;
-    for (int r=no; r >= 1; r--) {
+  int no; //declaring variables
+  long fac;
+
+  cout<<"Enter a number:"<<endl; //getting user input
+  cin>>no;
+
+  fac = 1; //initializing
+  
+  for (int r=no; r >= 1; r--) //repitition using for loop and calculating
+    {
         fac = fac * r;
     }
-
-    printf("Factorial of %d is %ld\n", no, fac);  
-    return 0;
+  cout<<"Factorial of  " << no << " is "<< fac <<endl;
+  
+  return 0;
 }
--- tute04.cpp
+++ tute04.cpp
@@ -16,14 +16,30 @@ Do not modify the main function.*/
 long Factorial(int no);
 long nCr(int n, int r);
 
-int main() {
+int main() 
+{
   int n, r;
   std::cout << "Enter a value for n ";
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
+  
+  std::cout << "nCr = "<<Factorial(n);
+  std::cout << "nCr : "<<nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+
+long Factorial(int no)
+{
+ int fac;
+ fac = no * Factorial(no - 1);
+ return fac;
+}
+
+long nCr(int n, int r)
+{
+int m;
+m = Factorial n/ Factorial r Factorial (n−r);
+return m;
 }
